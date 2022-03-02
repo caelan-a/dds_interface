83f03ee0d39845f406f2ddfc206e35bc9592e139
caelan-a <caelan.andsn@gmail.com>
2022-03-02 13:15:57 +1100

    windows batch file to automate static library build added

---
+++ scripts/build_static_lib.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+ECHO Building static library for DDS Interface on windows..
+set PROJECT_ROOT=%~dp0..
+ECHO "Project root: %PROJECT_ROOT%"
+ECHO:
+
+ECHO Creating build directory for cmake generated build files..
+cd %PROJECT_ROOT%
+if not exist "build" mkdir build
+cd %PROJECT_ROOT%/build
+
+ECHO Building static library with Debug config..
+cmake ../
+
+cmake --build . --config=Debug
+
+ECHO Building static library with Release config..
+cmake --build . --config=Release
+
+ECHO:
+ECHO Builds complete! Output libs can be found in /build/out..
