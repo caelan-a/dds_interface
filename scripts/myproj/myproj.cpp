6d3c704afe64b6f885ea482c391adbd4448b7980
caelan-a <caelan.andsn@gmail.com>
2022-03-03 03:29:33 +1100

    default working directory set for vs allowing debug config to find example_message.xml

--- scripts/myproj/myproj.cpp
+++ scripts/myproj/myproj.cpp
@@ -1,37 +1,18 @@
-#include "test_project.hpp"
-#include <string>
-#include <iostream>
-#include <vector>
-#include <string>
-#include <filesystem>
-#include <windows.h>
+#include "myproj.hpp"
 
-void read_directory(const std::string& name)
-{
-    std::string pattern(name);
-    pattern.append("\\*");
-    WIN32_FIND_DATA data;
-    HANDLE hFind;
-    if ((hFind = FindFirstFile(pattern.c_str(), &data)) != INVALID_HANDLE_VALUE) {
-        do {
-            std::cout << data.cFileName << "\n";
-        } while (FindNextFile(hFind, &data) != 0);
-        FindClose(hFind);
-    }
-}
 int main(int argc, char **argv) {
 
-    ////
-    ////  The following code demonstrates basic usage of the DDS Interface wrapper library!
-    ////
+    //
+    //  The following code demonstrates basic usage of the DDS Interface wrapper library!
+    //
 
     //  Use DDS Interface wrapper to create a convenient xml defined message publisher
     DDSInterface dds_interface = DDSInterface::CreateDDSInterface(0, 0);
     DDSPublisher dds_publisher = DDSPublisher::CreateDDSPublisher(dds_interface, 
-                                                                    "example_message.xml",  
+                                                                    "example_message.xml", 
                                                                     "example_topic", 
                                                                     "example_data_type", 
-                                                                    "example_data_type_library");
+                                                                    "example_data_type_library_yo");
 
     // Create message and set value
     dds::core::xtypes::DynamicData message = dds_publisher.create_message();
