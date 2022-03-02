c0d32b25425406829d75a2577155128641b69e50
caelan-a <caelan.andsn@gmail.com>
2022-03-02 13:36:22 +1100

    added precompiled libs and headers

--- scripts/create_dds_project.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-:: This script will generate a new CMake project with RTi DDS core libs and precompiled DDS Interface linked in  
-:: Uses example project as a template and replaces key parameters (eg project name)
-
-ECHO Creating new DDS enabled CMake project
-
