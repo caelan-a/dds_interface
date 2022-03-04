ff081cd6005a32ef70afcaaaa8ca51033ae482a9
Caelan Anderson <Caelan.Anderson@insitec.com.au>
2022-03-04 13:50:30 +1100

    fixed script to gen proj on linux

--- scripts/create_project_with_dds.sh
+++ scripts/create_project_with_dds.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "This script will generate a new CMake project with RTi DDS core libs and precompiled DDS Interface linked in"
 
-set CURRENT_DIR=$(pwd)
-set SCRIPT_LOCATION=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-set NEW_PROJECT_NAME=$1
+CURRENT_DIR=$(pwd)
+SCRIPT_LOCATION=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+NEW_PROJECT_NAME=$1
 
-set CMAKE_DIR="$SCRIPT_LOCATION/../cmake"
+CMAKE_DIR="$SCRIPT_LOCATION/../cmake"
 
 if [ -z "$NEW_PROJECT_NAME" ]; then
     echo Error: Project name not specified
@@ -20,6 +20,6 @@ then
 fi
 
 # Use cross platform CMake script for generating project 
-cmake -DPROJECT_NAME="%NEW_PROJECT_NAME%" -DPROJECT_PARENT_DIR="%CURRENT_DIR%" -P %CMAKE_DIR%/create_dds_enabled_project.cmake
+cmake -DPROJECT_NAME="$NEW_PROJECT_NAME" -DPROJECT_PARENT_DIR="$CURRENT_DIR" -P "$CMAKE_DIR/create_dds_enabled_project.cmake"
 
 
