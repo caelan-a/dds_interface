e82517483c5452f6e84712c31bcb2fe9564e2a59
Caelan Anderson <Caelan.Anderson@insitec.com.au>
2022-03-04 12:09:26 +1100

    fixed target in linux.cmake

--- cmake/linux.cmake
+++ cmake/linux.cmake
@@ -5,8 +5,8 @@ macro(configure_for_linux TARGET)
     find_package(Threads REQUIRED)
 
     # Add linux specific link libraries
-    target_link_libraries(${TARGET_DDS_INTERFACE_LIB_STANDALONE} PRIVATE Threads::Threads ${CMAKE_DL_LIBS})
+    target_link_libraries(${TARGET} PRIVATE Threads::Threads ${CMAKE_DL_LIBS})
 
     # Add linux specific preprocessor definitions
-    target_compile_definitions(${TARGET_DDS_INTERFACE_LIB_STANDALONE} PUBLIC UNIX; RTI_UNIX; NDDS_DLL_VARIABLE; _CONSOLE; _SCL_SECURE_NO_WARNINGS;)
+    target_compile_definitions(${TARGET} PUBLIC UNIX; RTI_UNIX; NDDS_DLL_VARIABLE; _CONSOLE; _SCL_SECURE_NO_WARNINGS;)
 endmacro()
