f25d435f5a7bdeb1231ae0e3a72698b614af4f82
Caelan Anderson <Caelan.Anderson@insitec.com.au>
2022-03-04 14:01:35 +1100

    added template copy for new shell scripts

--- cmake/create_dds_enabled_project.cmake
+++ cmake/create_dds_enabled_project.cmake
@@ -20,6 +20,8 @@ configure_file("${SCRIPT_DIR}/templates/new_project.cpp.in" ${PROJECT_DIR}/${NEW
 configure_file("${SCRIPT_DIR}/templates/new_project.hpp.in" ${PROJECT_DIR}/${NEW_PROJECT_NAME}.hpp @ONLY)
 configure_file("${SCRIPT_DIR}/templates/new_project_build.bat.in" ${PROJECT_DIR}/build.bat @ONLY)
 configure_file("${SCRIPT_DIR}/templates/new_project_setup.bat.in" ${PROJECT_DIR}/setup.bat @ONLY)
+configure_file("${SCRIPT_DIR}/templates/new_project_build.sh.in" ${PROJECT_DIR}/build.sh @ONLY)
+configure_file("${SCRIPT_DIR}/templates/new_project_setup.sh.in" ${PROJECT_DIR}/setup.sh @ONLY)
 configure_file("${SCRIPT_DIR}/templates/new_project_example_message.xml.in" ${PROJECT_DIR}/example_message.xml @ONLY)
 # Add in linux build script               
 
